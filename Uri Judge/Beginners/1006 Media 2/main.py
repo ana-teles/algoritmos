0018a76c186d59cdf104ab06255e5f21f2c61047
ana-teles <anateles16@yahoo.com.br>
2021-05-28 17:12:41 -0300

    Adicionadas soluções para os problemas 1003, 1006 e 1007 do URI

---
+++ Uri Judge/Beginners/1006 Media 2/main.py
@@ -0,0 +1,9 @@
+A = float(input())
+B = float(input())
+C = float(input())
+
+media = (A * 2 + B * 3 + C * 5) / 10
+
+print(f"MEDIA = {media:.1f}")
+
+
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-a = int(input())
-b = int(input())
-mul = a * b
-answer = f'multiplicação é igual a {mul}'
-print(answer)
+lista = [1, 7, 42, 13, 8]
+print(lista[3])
+soma = lista[0] + lista[4]
+print(soma)
+
+
+nomes = ["Vander", "Ana", "Luna", "Fumacinha"]
+print(nomes[1])
+print(nomes[0] + " ama " + nomes[1])
+print(f'{nomes[0]} ama {nomes[1]}')
+
+nomes = "Vander Ana Luna Fumacinha"
+lista_de_nomes = nomes.split()
+print(lista_de_nomes)
