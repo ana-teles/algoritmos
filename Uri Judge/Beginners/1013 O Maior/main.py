bd60c1ec92becbc34d0404cde9a8321d8c1da992
ana-teles <anateles16@yahoo.com.br>
2021-06-14 19:01:44 -0300

    Solução para os exercícios 1013 e 1014 do URI

---
+++ Uri Judge/Beginners/1013 O Maior/main.py
@@ -0,0 +1,8 @@
+numeros = input().split()
+numeros[0] = int(numeros[0])
+numeros[1] = int(numeros[1])
+numeros[2] = int(numeros[2])
+
+maior = max(numeros)
+
+print(f"{maior} eh o maior")
---
+++ Uri Judge/Beginners/1014 Consumo/main.py
@@ -0,0 +1,7 @@
+distancia = int(input())
+combustivel = float(input())
+
+
+media = distancia / combustivel 
+
+print(f"{media:.3f} km/l")
