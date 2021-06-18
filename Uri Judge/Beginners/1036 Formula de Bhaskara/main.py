32b39f2c06cbce9c24f46e8d2c98ff646591de2d
ana-teles <anateles16@yahoo.com.br>
2021-06-18 19:37:27 -0300

    Solução dos problemas 1036 e 1037 do URI

---
+++ Uri Judge/Beginners/1036 Formula de Bhaskara/main.py
@@ -0,0 +1,16 @@
+a, b, c = input().split()
+a = float(a)
+b = float(b)
+c = float(c)
+
+delta = b**2 - 4 * a * c
+
+if delta < 0 or a == 0:
+    print('Impossivel calcular')
+else:
+    r1 = (-b + delta ** 0.5) / (2 * a)
+    r2 = (-b - delta ** 0.5) / (2 * a)
+    print(f'R1 = {r1:.5f}')
+    print(f'R2 = {r2:.5f}')
+
+    
