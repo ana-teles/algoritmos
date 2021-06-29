2f63cd16e94b534cc96d666891d450b04e4e1fb1
ana-teles <anateles16@yahoo.com.br>
2021-06-29 18:09:28 -0300

    Solução dos problemas 1051  e 1052 do URI

--- Uri Judge/Beginners/1051 Imposto de Renda/main.py
+++ Uri Judge/Beginners/1051 Imposto de Renda/main.py
@@ -3,4 +3,16 @@ salario = float(input())
 taxa = 0 
 
 if salario > 4500.00:
-    
+    taxa += (salario - 4500.00) * 0.28
+    salario = 4500.00
+
+if salario >= 3000.01:
+    taxa += (salario - 3000.00) * 0.18
+    salario = 3000.00
+
+if salario >= 2000.01:
+    taxa += (salario - 2000.00) * 0.08
+    print(f"R$ {taxa:.2f}")
+
+else:
+    print("Isento")
---
+++ Uri Judge/Beginners/1052 Mes/main.py
@@ -0,0 +1,7 @@
+numero = int(input())
+
+meses = ("", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+
+meses[1] = "Janeiro"
+
+print(meses[numero])
